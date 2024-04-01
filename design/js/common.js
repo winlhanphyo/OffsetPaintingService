9740c9037c80c7330c2301d9736848504c3796dc
Yamin Thaw <yaminthaw54@gmail.com>
2024-04-01 21:23:59 +0630

    add design

--- design/js/common.js
+++ design/js/common.js
@@ -1,13 +1,22 @@
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 90) {
+        $('header').addClass('fixed');
+    }
+    else {
+        $('header').removeClass('fixed');
+    }
+});
+
 $(document).ready(function () {
     $('.g-nav li.has-menu a').on('click', function () {
         $(this).toggleClass('active');
         $('.sub-menu').toggleClass('is-show');
-        $('.sub-menu li').hover(function(){
-             $(this).find('ul').first().stop().toggle(200);
-             $('.dropdown-menu').addClass('menu-active');
-          }, function(){
-            $(this).find('ul').hide();
-            $('.dropdown-menu').removeClass('menu-active');
-          });
+        // $('.sub-menu li').hover(function(){
+        //   $(this).find('ul').first().stop().toggle(200);
+        //   $('.dropdown-menu').addClass('menu-active');
+        // }, function(){
+        //   $(this).find('ul').hide();
+        //   $('.dropdown-menu').removeClass('menu-active');
+        // });
     });
 });
