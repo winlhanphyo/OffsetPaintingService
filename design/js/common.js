c990565ef46fe961d3f00848d1735ecaf0c729db
Yamin Thaw <yaminthaw54@gmail.com>
2024-04-02 17:56:39 +0630

    add footer and responsive design

--- design/js/common.js
+++ design/js/common.js
@@ -11,12 +11,12 @@ $(document).ready(function () {
     $('.g-nav li.has-menu a').on('click', function () {
         $(this).toggleClass('active');
         $('.sub-menu').toggleClass('is-show');
-        // $('.sub-menu li').hover(function(){
-        //   $(this).find('ul').first().stop().toggle(200);
-        //   $('.dropdown-menu').addClass('menu-active');
-        // }, function(){
-        //   $(this).find('ul').hide();
-        //   $('.dropdown-menu').removeClass('menu-active');
-        // });
+    });
+});
+
+$(document).ready(function () {
+    $('.menu-toggle').on('click', function () {
+        $(this).toggleClass('active');
+        $('.gnav').toggleClass('is-show');
     });
 });
