59d8b70dd947ded13dd8f55a8e454321fd3d0826
winlhanphyo <winlhanphyo1@gmail.com>
2024-05-21 00:05:11 +0630

    banner migration and seeder

---
+++ backend/src/database/models/banner.model.ts
@@ -0,0 +1,62 @@
+import { DataTypes, Model, ModelAttributes } from "sequelize";
+import { associative } from './associate.decorator';
+import { DataBaseTableNames, DataBaseModelNames } from "../constants";
+import { DbModelFieldInit } from "../db-structure.model";
+import { db } from '../db.provider';
+
+export interface ICategoryModel {
+  id: number;
+  image: string;
+  createdUserId: number;
+  updatedUserId: number;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string;
+}
+
+const modelAttributes: DbModelFieldInit<Partial<ICategoryModel>> = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdUserId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'user',
+      key: 'id'
+    }
+  },
+  updatedUserId: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+    references: {
+      model: 'user',
+      key: 'id'
+    }
+  },
+};
+@associative
+export class BannerDbModel extends Model {
+  static associate({
+    UserDbModel
+  }: any) {
+    // this.belongsTo(UserDbModel);
+    this.belongsTo(UserDbModel, { foreignKey: 'createdUserId', as: 'createdBannerByUser', targetKey: 'id' });
+    this.belongsTo(UserDbModel, { foreignKey: 'updatedUserId', as: 'updatedBannerByUser', targetKey: 'id' });
+  }
+}
+
+BannerDbModel.init(modelAttributes as ModelAttributes, {
+  sequelize: db,
+  modelName: DataBaseModelNames.BANNER,
+  tableName: DataBaseTableNames.BANNER,
+  createdAt: 'createdAt',
+  updatedAt: 'updatedAt',
+  timestamps: true
+});
---
+++ backend/src/database/seeders/20240520073113-banner.seeder.ts
@@ -0,0 +1,42 @@
+'use strict';
+
+import { QueryInterface } from "sequelize";
+import { DataBaseTableNames } from "../constants";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface: QueryInterface, Sequelize: any) {
+    return queryInterface.bulkInsert(DataBaseTableNames.BANNER,
+      [
+        {
+          createdUserId: 1,
+          image: 'upload/user/banner/1.png',
+          createdAt: '2024-05-18',
+          updatedAt: '2024-05-18'
+        },
+        {
+          createdUserId: 1,
+          image: 'upload/user/banner/2.png',
+          createdAt: '2024-05-18',
+          updatedAt: '2024-05-18'
+        },
+        {
+          createdUserId: 1,
+          image: 'upload/user/banner/3.png',
+          createdAt: '2024-05-18',
+          updatedAt: '2024-05-18'
+        },
+        {
+          createdUserId: 1,
+          image: 'upload/user/banner/4.png',
+          createdAt: '2024-05-18',
+          updatedAt: '2024-05-18'
+        }
+      ], {});
+
+  },
+
+  down: (queryInterface: QueryInterface, Sequelize: any) => {
+    return queryInterface.bulkDelete(DataBaseTableNames.BANNER, {}, {});
+  }
+};
