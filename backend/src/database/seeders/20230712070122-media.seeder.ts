0653fcd587dd1bb2c982f8ece48368bbc2b5b38e
winlhanphyo <winlhanphyo1@gmail.com>
2024-09-14 13:51:03 +0630

    change product image

--- backend/src/database/seeders/20230712070122-media.seeder.ts
+++ backend/src/database/seeders/20230712070122-media.seeder.ts
@@ -9,22 +9,22 @@ module.exports = {
     return queryInterface.bulkInsert(DataBaseTableNames.MEDIA,
       [
         {
-          name: "TShirt 1",
-          url: 'upload/user/product/tshirt1.png',
+          name: "Product 1",
+          url: 'upload/user/product/1.png',
           status: "available",
           createdAt: '2023-07-12',
           updatedAt: '2023-07-12'
         },
         {
-          name: "TShirt 2",
-          url: 'upload/user/product/tshirt2.png',
+          name: "Product 2",
+          url: 'upload/user/product/2.png',
           status: "available",
           createdAt: '2023-07-12',
           updatedAt: '2023-07-12'
         },
         {
-          name: "TShirt 3",
-          url: 'upload/user/product/tshirt3.png',
+          name: "Product 3",
+          url: 'upload/user/product/3.png',
           status: "available",
           createdAt: '2023-07-12',
           updatedAt: '2023-07-12'
