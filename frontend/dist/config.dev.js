ebef9b6686f6df4a967da0b2384d01d98b3d22a6
winlhanphyo <winlhanphyo1@gmail.com>
2024-10-07 12:04:11 +0630

    change api url

--- frontend/dist/config.dev.js
+++ frontend/dist/config.dev.js
@@ -3,15 +3,15 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.version = exports.imgRoot = exports.loginRoot = exports.apiRoot = void 0;
-// export const apiRoot = 'https://api.cicimm.net/api/v1';
-var apiRoot = 'http://localhost:4000/api/v1'; // export const imgRoot = 'https://api.cicimm.net/';
+exports.version = exports.loginRoot = exports.imgRoot = exports.apiRoot = void 0;
+var apiRoot = 'https://api.cicimm.net/api/v1'; // export const apiRoot = 'http://localhost:4000/api/v1';
 
 exports.apiRoot = apiRoot;
-var loginRoot = 'http://localhost:4000/api'; // export const loginRoot = 'https://api.cicimm.net/api';
+var imgRoot = 'https://api.cicimm.net/'; // export const loginRoot = 'http://localhost:4000/api';
+
+exports.imgRoot = imgRoot;
+var loginRoot = 'https://api.cicimm.net/api'; // export const imgRoot = 'http://localhost:4000/';
 
 exports.loginRoot = loginRoot;
-var imgRoot = 'http://localhost:4000/';
-exports.imgRoot = imgRoot;
 var version = '1.24';
 exports.version = version;
