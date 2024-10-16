632568750b4feb39db136b3d6eee223dfff65d5b
winlhanphyo <winlhanphyo1@gmail.com>
2024-10-16 13:55:26 +0630

    dist folder

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
-// export const apiRoot = 'https://api.cicimm.net/api/v1';
-// export const loginRoot = 'https://api.cicimm.net/api';
-// export const imgRoot = 'https://api.cicimm.net/';
-export const apiRoot = 'http://localhost:4000/api/v1';
-export const loginRoot = 'http://localhost:4000/api';
-export const imgRoot = 'http://localhost:4000/';
+export const apiRoot = 'https://api.cicimm.net/api/v1';
+export const loginRoot = 'https://api.cicimm.net/api';
+export const imgRoot = 'https://api.cicimm.net/';
+// export const apiRoot = 'http://localhost:4000/api/v1';
+// export const loginRoot = 'http://localhost:4000/api';
+// export const imgRoot = 'http://localhost:4000/';
 export const version = '1.24';
